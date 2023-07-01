38a76c63587e9b591410742cba1be0abed63020e
diegosantarelli <s1099386@studenti.univpm.it>
2023-07-01 11:56:21 +0200

    track dal db

--- lib/screens/tracks/trackList_screen.dart
+++ lib/screens/tracks/trackList_screen.dart
@@ -1,84 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TracksListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/sfondo_schermata_circuiti.jpg'),
-              fit: BoxFit.cover,
-            ),
-        ),// Usa lo sfondo del tema
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: SizedBox(
-                height: 40,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Track name',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    prefixIcon: Icon(Icons.search),
-                  ),
-                ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('track').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+
+        final trackDocuments = snapshot.data!.docs;
+        final trackList = trackDocuments.map((doc) => doc['name']).toList();
+
+        return Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/sfondo_schermata_circuiti.jpg'),
+                fit: BoxFit.cover,
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: trackList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                        trackList[index],
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                    ),
-                    onTap: () {
-                      // Gestisci il clic su una traccia
-                    },
-                  );
-                },
-              ),
-            ),
-            Row(
+            child: Column(
               children: [
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Gestisci il clic su "Add track"
-                      },
-                      child: Text(
-                          'Add track',
-                        style: TextStyle(
-                          color: Colors.black
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: SizedBox(
+                    height: 40,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Track name',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
                         ),
+                        prefixIcon: Icon(Icons.search),
                       ),
                     ),
                   ),
                 ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: trackList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          trackList[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onTap: () {
+                          // Gestisci il clic su una traccia
+                        },
+                      );
+                    },
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Gestisci il clic su "Add track"
+                          },
+                          child: Text(
+                            'Add track',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
-
-final List<String> trackList = [
-  'Track 1',
-  'Track 2',
-  'Track 3',
-  // Aggiungi altri elementi della lista delle tracce come necessario
-];

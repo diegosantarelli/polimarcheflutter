2e04a342f4fe5552b0ab985e06837802f8c2c703
diegosantarelli <s1099386@studenti.univpm.it>
2023-07-02 12:10:24 +0200

    SetupTotal.dart

--- lib/screens/setup/SetupTotal.dart
+++ lib/screens/setup/SetupTotal.dart
@@ -55,7 +55,7 @@ class SetupScreen extends StatelessWidget {
             ),
             BottomNavigationBarItem(
               icon: Image.asset(
-                'assets/images/track.png',
+                'assets/images/setup_black.png',
                 width: 20,
                 height: 20,
               ),
